b604aec4c6ef5d28575e3aacb768b7924d29e2f8
root <root@TestDev.(none)>
2016-07-28 12:45:29 -0700

    json parser

--- json_parser.py
+++ json_parser.py
@@ -4,5 +4,6 @@ def convert_json():
     json_string = '{"first_name": "Guido", "last_name": "Rossum"}'
     parsed_json = json.loads(json_string)
     print (parsed_json['last_name'])
+    print "*&*"
 
 convert_json()
